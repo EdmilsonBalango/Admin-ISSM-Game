1bdd4460a9ac40285c3cf0bdb1e25eef908b3f6d
Edmilson Balango <edmilsonbalango34@gmail.com>
2021-05-11 17:28:32 +0200

    Main aplication for administrator

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const MainLogin = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{flex: 1, justifyContent: 'center', alignItems: 'center', display: 'flex'}}>
+      <p style={{color: '#444', fontWeight: 'bold', fontSize: 30}}>Bem-vindo ao issm game</p>
+
     </div>
+  );
+} 
+
+function App() {
+  
+  return (
+    <MainLogin />
   );
 }
 
